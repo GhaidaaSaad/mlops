c59b90a08b0601e5719c7eec0ec9d22e14ab6c2b
Ghaida <ghaidaSaadAlshehri@gmail.com>
2025-07-15 14:53:16 +0300

    First

---
+++ README.md
@@ -0,0 +1,2 @@
+# MLOps Abha Project
+This is a Git practice project created during the MLOps workshop.
---
+++ main.py
@@ -0,0 +1,9 @@
+def greet():
+    print("Hello from the 'first' branch!") #second
+
+def add(x,y):
+    return x + y
+
+if __name__ == "__main__":
+    greet()
+    print(add(5,3))
